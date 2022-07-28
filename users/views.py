ac5f6ec01340a34f1f6d842ca416e15ed732871d
Jande Royalty <janderoyalty@gmail.com>
2022-07-28 06:32:56 -0500

    changes delete to as like put

--- users/views.py
+++ users/views.py
@@ -35,14 +35,16 @@ def user_details(request, pk):
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    elif request.method == 'PUT':
+    elif request.method == 'PUT' or request.method == 'DELETE':
         serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        user.delete()
-        return Response(user.name, status=status.HTTP_204_NO_CONTENT)
+    # I don't want to record to be delete
+    # Added new active
+    # elif request.method == 'DELETE':
+    #     user.delete()
+    #     return Response(user.name, status=status.HTTP_204_NO_CONTENT)
 
